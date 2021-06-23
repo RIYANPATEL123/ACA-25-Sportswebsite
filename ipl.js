55bef56a436838aae25e69a6fef97a04d119a08c
RIYANPATEL123 <85875308+RIYANPATEL123@users.noreply.github.com>
2021-06-23 15:24:43 +0530

    Add files via upload

---
+++ ipl.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCB4EJUIMIxQaOUH0yfC3bx5uKY1VTG7Q8",
+    authDomain: "ipl-website-ade79.firebaseapp.com",
+    databaseURL: "https://ipl-website-ade79-default-rtdb.firebaseio.com",
+    projectId: "ipl-website-ade79",
+    storageBucket: "ipl-website-ade79.appspot.com",
+    messagingSenderId: "689602977727",
+    appId: "1:689602977727:web:8ebfc471d1f53e82d50b0a"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
